89ea1f5c09012a10f84be8dd42c0b59e8f46c463
Hugo Jesus <hugo.souza43@hotmail.com>
2025-11-05 23:30:53 -0300

    Primeiro escopo para o botão de interação do menu

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function openMenu() {
+  const menuMobile = document.getElementById("mobile-menu");
+
+  if (menuMobile.style.display === "block") {
+    menuMobile.style.display = "none";
+  } else {
+    menuMobile.style.display = "block";
+  }
+}
